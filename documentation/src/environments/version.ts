979af384ae10df3a5060e535e58522a161258081
Josh Kim <kjosh@vmware.com>
2021-08-25 13:14:23 -0700

    Fix memory leaks from animation and upgrade bus to 1.3.2
    Signed-off-by: Josh Kim <kjosh@vmware.com>

--- documentation/src/environments/version.ts
+++ documentation/src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "5986e4f-dirty",
-    "hash": "5986e4f",
+    "raw": "571f3c6-dirty",
+    "hash": "571f3c6",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "5986e4f-dirty",
+    "suffix": "571f3c6-dirty",
     "semverString": null,
-    "version": "1.0.0",
-    "time": "2021-08-21T00:35:51.207Z"
+    "version": "1.1.0",
+    "time": "2021-08-25T20:18:25.681Z"
 };
 /* tslint:enable */
