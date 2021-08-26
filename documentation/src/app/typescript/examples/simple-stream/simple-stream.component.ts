ec056fb73b96d5f8256e2f3af321426229f16e0e
Dave Shanley <dshanley@vmware.com>
2021-08-26 16:03:22 -0400

    Added TypeScript Examples to TS pages.
    Using new services available via plank. New simple stream, stock ticker and ping-pong bus examples.
    
    Signed-off-by: Dave Shanley <dshanley@vmware.com>

---
+++ documentation/src/app/typescript/examples/simple-stream/simple-stream.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'transport-simple-stream',
+  templateUrl: './simple-stream.component.html',
+  styleUrls: ['./simple-stream.component.scss']
+})
+export class SimpleStreamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
