84cdc4d2836fb0f0e742d50bbe14088916bcaf2d
Dave Shanley <dshanley@vmware.com>
2021-08-27 15:19:41 -0400

    Added changelog component and rendering.
    The changelog currently exists inside transport-typescript. This component extracts it and renders the data in a datagrid.
    
    Signed-off-by: Dave Shanley <dshanley@vmware.com>

---
+++ documentation/src/app/typescript/changelog/changelog.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import * as packageJSON from "@vmw/transport/package.json";
+import {ChangelogEntry} from "@vmw/transport-docs/shared/changelog/changelog.model";
+
+@Component({
+    selector: 'changelog-typescript',
+    templateUrl: './changelog.component.html',
+    styleUrls: ['./changelog.component.scss']
+})
+export class ChangelogComponent {
+   changelog: Array<ChangelogEntry> = [];
+
+    packageJSON: any = packageJSON;
+
+    ngOnInit() {
+        this.changelog = packageJSON.changelogHistory;
+    }
+
+}
