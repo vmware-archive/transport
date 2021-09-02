de5aad1b85f5162735ba51ff30cf9adac738fb46
Josh Kim <kjosh@vmware.com>
2021-09-02 14:49:01 -0700

    Add .eslintignore file for autogenerated files
    Signed-off-by: Josh Kim <kjosh@vmware.com>

--- documentation/src/environments/version.ts
+++ documentation/src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* eslint-disable */
+/* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "f98989b-dirty",
-    "hash": "f98989b",
+    "dirty": false,
+    "raw": "68a7850",
+    "hash": "68a7850",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "f98989b-dirty",
+    "suffix": "68a7850",
     "semverString": null,
     "version": "1.1.0",
-    "time": "2021-09-02T19:17:52.251Z"
+    "time": "2021-09-02T21:46:15.943Z"
 };
-/* eslint-enable */
+/* tslint:enable */
